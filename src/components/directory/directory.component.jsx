8ea750f923d88afc0091c7c68cdff8cbac7c9532
SivaKrishna6745 <krishnasiva6745@gmail.com>
2022-02-19 12:54:44 +0530

    updating code for shopping components and collection components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ class Directory extends Component {
                 {
                     title: 'hats',
                     imageURL: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
+                    id: 1,
+                    linkURL: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageURL: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
+                    id: 2,
+                    linkURL: ''
                 },
                 {
                     title: 'sneakers',
                     imageURL: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
+                    id: 3,
+                    linkURL: ''
                 },
                 {
                     title: 'women',
                     imageURL: 'https://i.ibb.co/GCCdy8t/women.png',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkURL: ''
                 },
                 {
                     title: 'men',
                     imageURL: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkURL: ''
                 }
             ]
         }
@@ -42,8 +47,8 @@ class Directory extends Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(section => {
-                        return <MenuItem key={section.id} title={section.title} imageURL={section.imageURL} size={section.size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => {
+                        return <MenuItem key={id} {...otherSectionProps}/>
                     })
                 }
             </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = (props) => {
+const MenuItem = ({title, imageURL, size, history, linkURL}) => {
     return (
-        <div className={`${props.size} menu-item`}>
-            <div className="background-image" style={{backgroundImage: `url(${props.imageURL})`}}></div>
+        <div className={`${size} menu-item`} onClick={() => {
+            history.push(`${linkURL}`);
+        }}>
+            <div className="background-image" style={{backgroundImage: `url(${imageURL})`}}></div>
             <div className="content">
-                <h1 className="title">{props.title.toUpperCase()}</h1>
+                <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">Shop Now</span>
             </div>
         </div>
     );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
